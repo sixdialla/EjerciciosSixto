eb246b0205cd66f0eefeef44faf0d9abdf11a17b
sixdialla <sidiazll_92@hotmail.com>
2017-11-20 14:18:37 +0100

    Modificaciones

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -13,5 +13,9 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
-
+   @RequestMapping("/bye")
+       public String bye(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+               model.addAttribute("name", name);
+	               return "bye";
+		           }
 }
